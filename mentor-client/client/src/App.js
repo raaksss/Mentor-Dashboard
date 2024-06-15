10b4b377643f5ab9082ca57c06ed244bd876a1bf
raaksss <rakshitamanocha1@gmail.com>
2024-06-15 22:34:17 +0530

    Student insights

--- mentor-client/client/src/App.js
+++ mentor-client/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Profile from './components/Profile';
+import StudentInsightPage from './components/StudentInsightPage';
 // Import other components here
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <div className="flex-1 p-4">
           <Routes>
             <Route path="/dashboard/profile" element={<Profile />} />
+            <Route path="/dashboard/add-class" element={<StudentInsightPage />} />
             
             {/* Define more routes as needed */}
           </Routes>
